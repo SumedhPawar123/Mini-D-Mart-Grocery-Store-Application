1c26e05ddcd31ee0f8ced8be977e366aa6a04682
Sumedh <psumedh59@gmail.com>
2026-08-23 18:50:31 +0530

    vercel deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
+// you can remove localhost
 
 // attach token from localStorage on every request
 api.interceptors.request.use((config) => {
